44103b8f29d3aaa710161010ce5d701f5f20ca14
alfredo-santiago5 <alfredo.santiago5@upr.edu>
2021-09-11 03:43:41 -0400

    Messing Around wiht app

--- Software/Dashboard/app.py
+++ Software/Dashboard/app.py
@@ -2,33 +2,7 @@
 
 import dash
 import dash_html_components as html
-import dash_core_components as dcc
+import math
 
-
-
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    dcc.Dropdown(
-        options=[
-            {'label': 'Home', 'value': 'HOME'},
-            {'label': 'Battery', 'value': 'BAT'},
-            {'label': 'Motor', 'value': 'MOT'},
-            {'label': 'Array', 'value': 'ARR'},
-        ],
-        value='HOME'
-    ),
-    html.Div(id='temetryMenu')
-])
-
-
-
-@app.callback(
-    dash.dependencies.Output(component_id= 'telemetryMenu', component_property = 'children'),
-    [dash.dependencies.Input('telemetryMenu', 'value')])
-def update_output(value):
-    return 0
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+print(math.sqrt(25))
 
