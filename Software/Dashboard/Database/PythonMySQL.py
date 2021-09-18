6b2a8c7ddd6be95655378a7a354bb855abaf308f
Carlos Machin <carlos.machin@gmail.com>
2021-09-18 00:15:23 -0400

    Succesfully connected to database.

--- Software/Dashboard/Database/PythonMySQL.py
+++ Software/Dashboard/Database/PythonMySQL.py
@@ -1,11 +1,13 @@
 import mysql.connector
 
-user = 'epiz_29758551'  # 'id17488151_esp32_serrt'
-password = 'teKA1nXlygs'  # 'solarpower' #'Solarpower2021$'
-host = 'sql203.epizy.com'  # 'localhost'  # '48564.us-imm-sql6.000webhost.io'
-database = 'epiz_29758551_esp32'  # 'id17488151_esp32'
+user = 'telemetry'  # 'id17488151_esp32_serrt'
+password = 'solarpower'  # 'solarpower' #'Solarpower2021$'
+host = 'db4free.net'  # 'localhost'  # '48564.us-imm-sql6.000webhost.io'
+database = 'esp32data'  # 'id17488151_esp32'
 port = '3306'
 
 cnx = mysql.connector.connect(user=user, password=password, host=host, database=database, port=port)
+
+print("Connected to " + database)
 
 cnx.close()
