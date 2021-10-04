0e0d14c08850a8ea2ab1ecfc988951cd094fe85a
cmachin <carlos.machin@upr.edu>
2021-10-04 13:48:50 -0400

    Connected to database

--- Software/Database/PythonMySQL.py
+++ Software/Database/PythonMySQL.py
@@ -1,28 +1,13 @@
 import mysql.connector
 
-user = 'Administrator'
+user = 'admin'
 password = 'Solarpower802'
 host = 'serrt-database.c7sgroillrv6.us-east-2.rds.amazonaws.com'
-database = 'serrt-database'
+database = 'sensor_data'
 port = '3306'
 
 db = mysql.connector.connect(user=user, password=password, host=host, database=database, port=port)
 
 print("Connected to database!")
 
-# get cursor object
-cursor= db.cursor()
-  
-# execute your query
-cursor.execute("SELECT * FROM SensorData")
-  
-# fetch all the matching rows 
-result = cursor.fetchall()
-  
-# loop through the rows
-for row in result:
-    print(row)
-    print("\n")
-
-cursor.close()
 db.close()
